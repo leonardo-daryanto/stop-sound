1267834f66a343233b393ef4eab93df6b62439bc
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-05-14 09:25:39 +0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
+//% weight=100 icon=M color=#AF7AC5
 
+namespace melody {
+    /**
+     *  stop the melody
+     */
+    //% block
+    export function stopMelody(): void {
+
+        forever(function() {
+            
+            music.rest(BeatFraction.Whole)
+        })
+    }
+}
